95c92643cb234db1f88ca7f21e279363091891bc
BuildTools <unconfigured@null.spigotmc.org>
2025-06-13 15:07:37 -0700

    fixing urls attempt 1

--- resources/views/components/metadataHead.blade.php
+++ resources/views/components/metadataHead.blade.php
@@ -7,11 +7,11 @@
     <meta name="description" content="@yield('description', 'Somos una compañia enfocada en marketing digital basada en Nogales Sonora')">
     <meta name="keywords" content="@yield('keywords', 'Nogales, paginas web, marketing, diseño, personalización, marketing digital, grabado láser, uniformes bordados, puntos de venta')">
     <meta name="author" content="José Padilla">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="{{ url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css') }} ">
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
-    <script src="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> 
-    <link href="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.snow.css" rel="stylesheet">
+    <script src="{{ url('https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js') }}"></script>
+    <script src="{{ url('https://cdn.jsdelivr.net/npm/chart.js') }}"></script> 
+    <link href="{{ url('https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.snow.css') }}" rel="stylesheet">
     @vite(['resources/css/app.css'])
     @yield('styles')
 </head>
